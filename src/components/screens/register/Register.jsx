c7268895125439a8be89368e7b045b80c4e39eaf
GeraldineRomer <geraldine.rome0104@gmail.com>
2024-09-28 07:04:38 -0500

    validations forms [register] [login]

--- src/components/screens/register/Register.jsx
+++ src/components/screens/register/Register.jsx
@@ -3,17 +3,84 @@ import  Logo  from '../../../assets/Logos_UAM-07.png';
 import { Button, Checkbox, Col, Form, Input, Row } from 'antd';
 import './Register.css';
 import { Link } from 'react-router-dom';
-
-const onFinish = (values) => {
-    console.log('Success:', values);
-};
-
-const onFinishFailed = (errorInfo) => {
-    console.log('Failed:', errorInfo);
-};
+import { useFormik } from 'formik';
 
 export const register = () => {
-  return (
+    const validate = values => {
+        const errors = {};
+        
+        //email validation
+        if (!/^[A-Z0-9._%+-]+@autonoma\.edu\.co$/i.test(values.email)) {
+            errors.email = 'Correo inválido, debe ser dominio @autonoma';
+        } else if (!values.email){
+            errors.email = 'Este campo es requerido';
+        }
+        
+        //pasword validation
+        if (values.password.length < 8){
+            errors.password = 'La contraseña debe tener más de 8 dígitos'
+        } else if (!values.password){
+            errors.password = 'Este campo es requerido';
+        }
+
+        // repassword validation
+        if (values.password !== values.repassword){
+            errors.repassword = 'Las contraseñas no coinciden'
+        } else if (!values.password){
+            errors.repassword = 'Este campo es requerido';
+        }
+        
+        //name validation 
+        if(!/^[A-Za-zÁÉÍÓÚáéíóúñÑüÜ\s]+$/.test(values.name)){
+            errors.name = 'El nombre solo contiene letras'
+        } else if (!values.name ){
+            errors.name = 'Este campo es requerido';
+        }
+        
+        //lastname validation
+        if(!/^[A-Za-zÁÉÍÓÚáéíóúñÑüÜ\s]+$/.test(values.lastname)){
+            errors.lastname = 'El apellido solo contiene letras'
+        } else if (!values.lastname){
+            errors.lastname = 'Este campo es requerido';
+        }
+        
+        //program validation
+        if(!/^[A-Za-zÁÉÍÓÚáéíóúñÑüÜ\s]+$/.test(values.program)){
+            errors.program = 'Este campo es requerido';
+        } else if (!values.program){
+            errors.program = 'El programa solo contiene letras'
+        }
+
+        // id validation
+        if (!/^[0-9]+$/.test(values.id)){
+            errors.id = 'La identificación es con números'
+        } else if(!values.id){
+            errors.repassword = 'Este campo es requerido';
+        }
+
+        // checkbox validation
+        if (!values.terms) {
+            errors.terms = 'Debes aceptar los términos y condiciones';
+        }
+
+        return errors;
+    };
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+            password: '',
+            name: '',
+            lastname: '',
+            repassword: '',
+            program: '',
+            id: ''
+        },
+        validate,
+        onSubmit: values => {
+            alert(JSON.stringify(values, null, 2));
+        }
+    });
+    return (
     <div className='register'>
         <img src={Logo} className='register__logo'/>
         <h2 className='register__title'>Regístrate en Rescate UAM</h2>
@@ -22,104 +89,114 @@ export const register = () => {
             labelCol={{
                 span: 8,
             }}
-            initialValues={{
-                remember: true,
-            }}
-            onFinish={onFinish}
-            onFinishFailed={onFinishFailed}
             autoComplete="off"
+            onSubmit={formik.handleSubmit}
         >   
             <Row gutter={16}>
                 <Col>
-                    <Form.Item
-                        rules={[
-                            {
-                            required: true,
-                            message: '¡Ingresa tu correo!',
-                            },
-                        ]}
-                        >
-                        <Input placeholder="Correo UAM" className='register__input'/>
+                    <Form.Item>
+                        <Input 
+                            placeholder="Correo UAM" 
+                            className='register__input'
+                            id="email"
+                            name="email"
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                            value={formik.values.email}
+                        />
+                        {formik.touched.email && formik.errors.email ? (<div className='register__error'>{formik.errors.email}</div>) : null}
                     </Form.Item>
                 </Col>
                 <Col>
-                    <Form.Item
-                        rules={[
-                            {
-                            required: true,
-                            message: '¡Ingresa tu nombre!',
-                            },
-                        ]}
-                        >
-                        <Input placeholder="Nombre" className='register__input'/>
+                    <Form.Item>
+                        <Input 
+                            placeholder="Nombre" 
+                            className='register__input'
+                            id="name"
+                            name="name"
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                            value={formik.values.name}
+                            />
+                            {formik.touched.name && formik.errors.name ? (<div className='register__error'>{formik.errors.name}</div>) : null}
                     </Form.Item>
                     
                 </Col>
             </Row>
             <Row gutter={16}>
                 <Col >
-                    <Form.Item
-                        rules={[
-                            {
-                            required: true,
-                            message: '¡Ingresa tu contraseña!',
-                            },
-                        ]}
-                        >
-                        <Input.Password placeholder='Contraseña' className='register__input'/>
+                    <Form.Item>
+                        <Input.Password 
+                            placeholder='Contraseña' 
+                            className='register__input'
+                            id="password"
+                            name="password"
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                            value={formik.values.password}
+                            />
+                            {formik.touched.password && formik.errors.password ? (<div className='register__error'>{formik.errors.password}</div>) : null}
                     </Form.Item>
                     
                 </Col>
                 <Col>
-                    <Form.Item
-                        rules={[
-                            {
-                            required: true,
-                            message: '¡Ingresa tu apellido!',
-                            },
-                        ]}
-                        >
-                        <Input placeholder="Apellido" className='register__input'/>
+                    <Form.Item>
+                        <Input 
+                            placeholder="Apellido" 
+                            className='register__input'
+                            id="lastname"
+                            name="lastname"
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                            value={formik.values.lastname}
+                            />
+                            {formik.touched.lastname && formik.errors.lastname ? (<div className='register__error'>{formik.errors.lastname}</div>) : null}
                     </Form.Item>
                 </Col>
             </Row>
             <Row gutter={16}>
                 <Col>
-                    <Form.Item
-                        rules={[
-                            {
-                            required: true,
-                            message: 'Repite tu contraseña!',
-                            },
-                        ]}
-                        >
-                        <Input.Password placeholder='Repetir Contraseña' className='register__input'/>
+                    <Form.Item>
+                        <Input.Password 
+                            placeholder='Repetir Contraseña' 
+                            className='register__input'
+                            id="repassword"
+                            name="repassword"
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                            value={formik.values.repassword}
+                            />
+                            {formik.touched.repassword && formik.errors.repassword ? (<div className='register__error'>{formik.errors.repassword}</div>) : null}
                     </Form.Item>
                 </Col>
                 <Col>
-                    <Form.Item
-                        rules={[
-                            {
-                            required: true,
-                            message: '¡Ingresa tu programa!',
-                            },
-                        ]}
-                        >
-                        <Input placeholder="Programa" className='register__input'/>
+                    <Form.Item>
+                        <Input 
+                            placeholder="Programa" 
+                            className='register__input'
+                            id="program"
+                            name="program"
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                            value={formik.values.program}
+                            />
+                            {formik.touched.program && formik.errors.program ? (<div className='register__error'>{formik.errors.program}</div>) : null}
                     </Form.Item>
                 </Col>
             </Row>
             <Row>
                 <Col span={24} className='register__inputs'>
-                    <Form.Item
-                        rules={[
-                            {
-                            required: true,
-                            message: '¡Ingresa tu número de identidad!',
-                            },
-                        ]}
-                        >
-                        <Input placeholder="Cédula/Tarjeta Identidad" className='register__input'/>
+                    <Form.Item>
+                        <Input 
+                            placeholder="Cédula/Tarjeta Identidad" 
+                            className='register__input'
+                            id="id"
+                            name="id"
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                            value={formik.values.id}
+                            />
+                            {formik.touched.id && formik.errors.id ? (<div className='register__error'>{formik.errors.id}</div>) : null}
                     </Form.Item>
                 </Col>
             </Row>
@@ -129,19 +206,25 @@ export const register = () => {
                         name="remember"
                         valuePropName="unchecked"
                     >
-                        <Checkbox>
+                        <Checkbox
+                            name="terms"
+                            checked={formik.values.terms}
+                            onChange={e => formik.setFieldValue('terms', e.target.checked)} 
+                            onBlur={formik.handleBlur}
+                        >
                             Aceptar{' '}
                             <Link className='register__terms'>
                                 términos y condiciones
                             </Link>
                         </Checkbox>
+                        {formik.touched.terms && formik.errors.terms ? (<div className='register__error'>{formik.errors.terms}</div>) : null}
                     </Form.Item>
                 </Col>
             </Row>
             <Row>
                 <Col span={24} className='register__inputs'>
                     <Form.Item>
-                        <Button htmlType="submit" className='register__button'>
+                        <Button htmlType="submit" className='register__button' type="submit">
                             Aceptar
                         </Button>
                     </Form.Item>
