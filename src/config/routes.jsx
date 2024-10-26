40c6f6fcb2ab3152848767300ddbc63535c710c3
yansaid21 <yansaid21@gmail.com>
2024-10-26 17:52:35 -0500

    spinner added. spinner is working and the images are fixed in the big emergency button, and the complete register appear in the main screen, and not in the login screen [Spinner.jsx, Login.jsx, BigEmergencyButton.jsx]

--- src/config/routes.jsx
+++ src/config/routes.jsx
@@ -1,8 +1,8 @@
 import { Login } from "../components/screens/login/login";
 import { Main } from "../components/screens/main/Main";
 import { Register } from "../components/screens/register/Register";
+/* import {Spinner} from "../components/atoms/Spinner/Spinner";  */
 //import { CompleteRegister } from "../components/atoms/CompleteRegister";
-
 export const GeneralRoutes = [
     { path: "/", component: Login},
     { path: "/register", component: Register},
