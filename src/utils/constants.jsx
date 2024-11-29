9217b99b0abba5657dd89d49edf107071e8d6b13
GeraldineRomer <geraldine.rome0104@gmail.com>
2024-11-29 14:31:21 -0500

    cursor pointer icons edit

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,5 +1,5 @@
-const SERVER_IP = "localhost:8000";
-const SECURE = false;
+const SERVER_IP = "rescueapi.xyz";
+const SECURE = true;
 export const SECRET_KEY =
   "quiñones:yXaNffqVUT2z9v6ESuJ/t8FJNWRAhbQWMNGR9Vcm1u0=";
 // server dns rescueapi.xyz
